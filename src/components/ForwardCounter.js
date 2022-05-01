330cd9fb218352a5a5cd70934b7af3415a49b012
jonascolas <jonas@fusemachines.com>
2022-05-01 10:48:58 -0300

    useCounter custom hooks

--- src/components/ForwardCounter.js
+++ src/components/ForwardCounter.js
@@ -1,8 +1,10 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+import useCounter from '../hooks/use-counter';
 
 import Card from './Card';
 
 const ForwardCounter = () => {
+  const counter = useCounter();
   // const [counter, setCounter] = useState(0);
 
   // useEffect(() => {
