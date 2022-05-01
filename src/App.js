d308a42d0e21df0cb688d56d31c8716b392d831a
jonascolas <jonas@fusemachines.com>
2022-05-01 13:16:46 -0300

    custom hook with get method

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import BackwardCounter from './components/counter/BackwardCounter';
-import ForwardCounter from './components/counter/ForwardCounter';
+import React, {Fragment, useEffect, useState } from 'react';
+// import BackwardCounter from './components/counter/BackwardCounter';
+// import ForwardCounter from './components/counter/ForwardCounter';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
+
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const transformTasks = (tasksObj) => {
+    const loadedTasks = [];
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+    }
+    setTasks(loadedTasks);
+  };
+
+  const { isLoading, error, sendRequest: fetchTasks }  = useHttp({url: process.env.REACT_APP_TASKS_LINK}, transformTasks);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
   return (
-    <React.Fragment>
-      <ForwardCounter />
-      <BackwardCounter />
-    </React.Fragment>
+    <Fragment>
+      {/* <ForwardCounter /> */}
+      {/* <BackwardCounter /> */}
+
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+
+    </Fragment>
   );
 }
 
